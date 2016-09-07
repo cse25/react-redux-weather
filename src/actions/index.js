fb9b368fa8f47af1955c4e23942f8478a3d9ba3d
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-09-07 16:22:36 -0700

    setup fetchWeather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
 import axios from 'axios';
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  // request is a promise to pass into the action as the payload
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}

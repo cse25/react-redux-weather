dad5530e238e7e9b3011ff303245a215dbe48bc8
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-09-09 13:09:30 -0700

    add averages to chart

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(array) {
+  const length = array.length
+  const sum = array.reduce((a,b) => {
+    return a + b
+  })
+  return Math.round(sum / length);
+}
 
 export default (props) => {
   return (
     <div>
       <Sparklines height={120} width={180} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{average(props.data)}</div>
     </div>
   )
 }

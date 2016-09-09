280a6c6088211c054426b24525c75a7466c95624
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-09-09 15:03:04 -0700

    convert K to F

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,14 +5,16 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp)
+    const temps = cityData.list.map(weather => weather.main.temp).map((temp) => {
+      return temp * (9/5) - 450.67;
+    })
     const pressures = cityData.list.map(weather => weather.main.pressure)
     const humidities = cityData.list.map(weather => weather.main.humidity)
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td><Chart data={temps} color={"orange"} units="K" /></td>
+        <td><Chart data={temps} color={"orange"} units="F" /></td>
         <td><Chart data={pressures} color={"blue"} units="kPa" /></td>
         <td><Chart data={humidities} color={"green"} units="%" /></td>
       </tr>
@@ -25,7 +27,7 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature (K)</th>
+            <th>Temperature (F)</th>
             <th>Pressure (hPa)</th>
             <th>Humidity (%)</th>
           </tr>
